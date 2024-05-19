3f934ae2c91f502ce9d61b09d9df5b87e6fac927
ilyas99 <e2346104@cmaisonneuve.qc.ca>
2024-05-18 22:51:49 -0400

    calcul distance entre deux points geographique

---
+++ partie33.py
@@ -0,0 +1,11 @@
+import math
+
+def calculer_distance(lat1, lon1, lat2, lon2):
+    R = 6371  # Rayon de la Terre en kilomètres
+    phi1, phi2 = math.radians(lat1), math.radians(lat2)
+    delta_phi = math.radians(lat2 - lat1)
+    delta_lambda = math.radians(lon2 - lon1)
+    a = math.sin(delta_phi/2)**2 + math.cos(phi1) * math.cos(phi2) * math.sin(delta_lambda/2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c
+    return distance
